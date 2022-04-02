1ae4f830ec48c6c55e07f5c9b2bf58aad5ad03ff
Kc pele <fidekg123@gmail.com>
2022-04-02 05:54:06 +0000

    started fetching the data

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
-import express  from "express";
+import express from "express";
+import products from "./data/Products.js";
 
+const app = express();
 
-const app =  express()
-
+//loading product
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+    const product  = products.find((id) => id._id === req.params.id);
+    res.json(product);
+  });
 app.get("/", (req, res) => {
-    res.send('Api runing')
-})
+  res.send("Api");
+});
 
-
-app.listen(5000, console.log("server runing on port 5000"))
+app.listen(5000, console.log("server runing on port 5000"));
